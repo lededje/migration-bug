2cd260040e6cab413c8ae6cfb2d5a51cdbb9cd51
Miles Spoor <git@likeminded.io>
2022-03-15 11:02:46 +0100

    Adds broken migration example

---
+++ index.js
@@ -0,0 +1,77 @@
+const { Sequelize, Model, DataTypes } = require('sequelize');
+const pg = require('pg');
+const { Umzug, SequelizeStorage } = require('umzug');
+
+const sequelize = new Sequelize('postgres://postgres@localhost/test', {
+  dialect: 'postgres',
+  dialectModule: pg,
+  dialectOptions: {
+    ssl: false,
+  },
+  define: {
+    underscored: true,
+  },
+  logging: false,
+});
+
+const umzug = new Umzug({
+  migrations: [
+    {
+      name: '0001-create-tags',
+      up: async ({ context: queryInterface }) => {
+        return queryInterface.createTable('tags', {
+          id: {
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+            type: DataTypes.INTEGER,
+          },
+          type: {
+            type: DataTypes.ENUM(
+              'NEW',
+              'ORIGINAL',
+              'DUPLICATE',
+            ),
+            allowNull: false,
+          },
+          created_at: {
+            allowNull: false,
+            type: DataTypes.DATE,
+          },
+          updated_at: {
+            allowNull: false,
+            type: DataTypes.DATE,
+          },
+        });
+      },
+      down: async ({ context: queryInterface }) => {
+        return queryInterface.dropTable('tags');
+      },
+    }
+  ],
+  context: sequelize.getQueryInterface(),
+  storage: new SequelizeStorage({ sequelize }),
+  logger: console,
+});
+
+
+class Tags extends Model {};
+
+Tags.init({
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  type: DataTypes.ENUM(
+    'NEW',
+    'ORIGINAL',
+    'DUPLICATE',
+  ),
+}, { sequelize, modelName: 'tags' });
+
+(async () => {
+  await umzug.up();
+
+  await umzug.down({ to: 0 });
+})();
